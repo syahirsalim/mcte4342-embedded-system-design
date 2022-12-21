235aaa646ddb2855707f77ac1d1607db05001148
syahirsalim <77923513+syahirsalim@users.noreply.github.com>
2022-12-21 20:11:26 +0800

    Add files via upload

---
+++ Week 5/Example 1/Week_5_Ex1.ino
@@ -0,0 +1,14 @@
+
+
+
+
+void setup() {
+Serial.begin(9600);
+
+}
+
+void loop() {
+ int value = analogRead(A0);
+ Serial.println(value);
+
+}
---
+++ Week 5/Example 2/Week_5_Ex2.ino
@@ -0,0 +1,23 @@
+char *admux = (char*) 0x7C; //Signed/unsigned does not matter if you don’t do decimal work
+volatile unsigned char *adcsra = (unsigned char*) 0x7A;
+volatile unsigned char *adch = (unsigned char*) 0x79;
+volatile unsigned char *adcl = (unsigned char*) 0x78;
+void setup()
+{
+*admux = 0b01000011; //Set analog reference to AVCC and analog source to PC3.
+*adcsra = 0b11000111; //Enable ADC, start conversion and set pre-scaler to 128
+Serial.begin(9600);
+while(1)
+{
+*adcsra |= 0b01000000; //Start conversion (Set ADSC bit)
+while (((*adcsra) & 0b01000000)) //As long as ADSC bit is HIGH
+{
+Serial.println("nothing");
+}
+int lowbyte = (*adcl); //Capture low byte
+int highbyte = (*adch); //Capture high byte
+int value = (highbyte<<8) | lowbyte; //Merge high byte and low byte
+Serial.println(value);
+delay(1000);
+}
+}
